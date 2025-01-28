2cd2aca2bf9426ab0dd59416028cb0277d36d279
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-28 23:38:21 +0600

    initialte the main class.

--- functions.php
+++ functions.php
@@ -1,5 +1,50 @@
 <?php
+/**
+ * Techspace functions and definitions
+ *
+ * @link https://github.com/vxlrubel/techspace
+ * @author Rubel Mahmud
+ * @package Techspace
+ * @since 1.0
+ */
+
+//  directly access denied
+ defined('ABSPATH') || exit;
+
 // Load Composer autoloader
 if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
+
+
+final class Techspace{
+
+
+    // instance of the class
+    private static $instance;
+
+    // theme version
+    public static $version = '1.0';
+    
+    public function __construct(){
+        
+    }
+
+    /**
+     * create a new instance
+     *
+     * @return void
+     */
+    public static function init(){
+        if( is_null( self::$instance ) ){
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+}
+
+function techspace(){
+    return Techspace::init();
+}
+techspace();
