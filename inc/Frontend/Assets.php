a7afba8bf1b2739a5682c043865cde6ecf7ab429
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-28 23:42:40 +0600

    create Assets class for load script and style.

---
+++ inc/Frontend/Assets.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Techspace functions and definitions
+ *
+ * @link https://github.com/vxlrubel/techspace
+ * @author Rubel Mahmud
+ * @package Techspace
+ * @since 1.0
+ */
+
+namespace Techspace\Frontend;
+//  directly access denied
+defined('ABSPATH') || exit;
+
+class Assets{
+    public function __cnnstruct(){
+        
+    }
+}
