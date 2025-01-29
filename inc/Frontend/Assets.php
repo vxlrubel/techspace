8ae9d16d6a1677dbc138ccf7f129ea73b0900415
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-29 23:45:50 +0600

    register script and style.

--- inc/Frontend/Assets.php
+++ inc/Frontend/Assets.php
@@ -13,9 +13,23 @@ namespace Techspace\Frontend;
 defined('ABSPATH') || exit;
 
 class Assets{
+
+    // stylesheet directory
+    private $style_directory = 'css/';
+
+    // scripts directory
+    private $script_directory = 'js/';
+    
     public function __construct(){
         // remove block livrary css
         add_action( 'wp_enqueue_scripts', [$this, 'remove_wp_block_library_css'], 100 );
+
+        // register stylesheets
+        add_action( 'wp_enqueue_scripts', [ $this, 'register_stylesheets' ] );
+
+        // register scripts
+        add_action( 'wp_enqueue_scripts', [ $this, 'register_scripts' ] );
+
     }
 
     /**
@@ -27,4 +41,89 @@ class Assets{
         wp_dequeue_style('wp-block-library-theme'); 
         wp_dequeue_style('global-styles');
     }
+    
+    /**
+     * register scripts
+     *
+     * @return void
+     */
+    public function register_scripts(){
+        $get_scripts = $this->get_scripts();
+
+        foreach ( $get_scripts as $handle => $script ){
+            wp_enqueue_script(
+                $handle,
+                $script['src'],
+                $script['deps'],
+                TS_THEME_VERSION,
+                true
+            );
+        }
+
+        $args = [
+            'ajax_url' => admin_url( 'admin-ajax.php' )
+        ];
+        
+        wp_localize_script( 'ts-script', 'TS', $args );
+    }
+
+    /**
+     * register stylesheets
+     *
+     * @return void
+     */
+    public function register_stylesheets(){
+        $get_style = $this->get_styles();
+
+        foreach ( $get_style as $handle => $style ){
+            $deps = isset( $style['deps'] ) ? $style['deps'] : '';
+            wp_enqueue_style(
+                $handle,
+                $style['src'],
+                $deps,
+                TS_THEME_VERSION,
+                'all'
+            );
+        }
+    }
+
+     /**
+     * get the stylesheets
+     *
+     * @return $stylesheets
+     */
+    public function get_styles(){
+        $stylesheets = [
+            'ts-style' => [
+                'src' => TS_ASSETS . $this->style_directory . 'main.css'
+            ]
+        ];
+
+        $stylesheets = apply_filters( 'ts_style', $stylesheets );
+
+        return $stylesheets;
+    }
+
+    /**
+     * get scripts
+     *
+     * @return void
+     */
+    public function get_scripts(){
+        $scripts = [
+            'vuejs' => [
+                'src'  => TS_ASSETS . $this->script_directory . 'vue.global.prod.js',
+                'deps' => []
+            ],
+            'ts-script' => [
+                'src'  => TS_ASSETS . $this->script_directory . 'custom.js',
+                'deps' => ['vuejs']
+            ]
+        ];
+
+        $scripts = apply_filters( 'ts_scripts', $scripts );
+        
+        return $scripts;
+    }
+
 }
