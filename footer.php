3fbf9c4cab5d996c8b3e6d96253e673b3942c386
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-31 21:28:45 +0600

    create footer for manage all the footer section .

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php defined('ABSPATH') || exit; ?>
+
+<footer class="clearfix footer-area" id="siteFooter">
+    <div class="copyrigh-area text-center h-100">
+        <div class="container h-100">
+            <div class="d-flex align-items-center flex-wrap justify-content-center h-100 gap-1">
+                Copyright @2025. All right reserved by <a href="javascript:void(0)" class="text-dark text-decoration-none fw-bold">Techspace</a>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
