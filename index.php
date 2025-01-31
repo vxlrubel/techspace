3023c01bbc533219bb4db1a65f1e8ee16c1139e7
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-31 19:59:03 +0600

    detouch header & footer and also add the logo dynamically.

--- index.php
+++ index.php
@@ -1,41 +1,14 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
 
-    <?php
-        apply_filters( 'ts_google_map', true );
-        wp_head();
-    ?>
-</head>
-<body>
+//  directly access denied
+defined('ABSPATH') || exit;
 
-    <header class="clearfix header-area text-primary">
-        <div class="container">
-            <strong>Header: </strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officia soluta officiis magni vitae reiciendis!
-        </div>
-    </header>
+get_header(); ?>
 
-    <main class="main-area">
-        <div class="container">
-            <strong>Define Main Area</strong>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo neque odit voluptatem omnis facere impedit!</p>
-        </div>
-    </main>
-
-
-    <footer class="clearfix footer-area">
-        <div class="copyrigh-area text-center h-100">
-            <div class="container h-100">
-                <div class="d-flex align-items-center flex-wrap justify-content-center h-100 gap-1">
-                    Copyright @2025. All right reserved by <a href="javascript:void(0)" class="text-dark text-decoration-none fw-bold">Techspace</a>
-                </div>
-            </div>
-        </div>
-    </footer>
-    
-    
-    <?php wp_footer(); ?>
-</body>
-</html>
+<main class="main-area" id="mainContent">
+    <div class="container">
+        <strong>Define Main Area</strong>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo neque odit voluptatem omnis facere impedit!</p>
+    </div>
+</main>
+<?php get_footer(); ?>
