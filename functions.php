03369f0362c196d90a4acc1b65a6c53817428fec
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-31 18:23:26 +0600

    update function for setup theme.

--- functions.php
+++ functions.php
@@ -36,8 +36,72 @@ final class Techspace{
             add_action( 'init', [ $this, 'remove_emoji_styles'] );
         }
 
+        // theme setup
+        add_action( 'after_setup_theme', [ $this, 'setup_theme' ] );
+
     }
 
+    /**
+     * theme setup
+     * 
+     * @return void
+     */
+    public function setup_theme(){
+        
+        // Enable title tag support
+        add_theme_support( 'title-tag' );
+
+        // Enable post thumbnails
+        add_theme_support( 'post-thumbnails' );
+
+        // Enable custom logo
+        add_theme_support( 'custom-logo', [
+            'height'      => 28,
+            'width'       => 120,
+            'flex-height' => true,
+            'flex-width'  => true,
+            'header-text' => [ 'site-title', 'site-description' ]
+        ] );
+
+        // Enable HTML5 markup support
+        add_theme_support( 'html5', [
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ] );
+
+        // Enable custom background
+        add_theme_support( 'custom-background', [
+            'default-color' => 'ffffff',
+            'default-image' => '',
+        ] );
+
+        // Enable custom header
+        add_theme_support( 'custom-header', [
+            'default-image'      => '',
+            'default-text-color' => '#444444',
+            'width'              => 1200,
+            'height'             => 400,
+            'flex-width'         => true,
+            'flex-height'        => true,
+        ] );
+
+        // Enable automatic feed links
+        add_theme_support( 'automatic-feed-links' );
+
+        // Enable wide and full alignment support
+        add_theme_support( 'align-wide' );
+
+        // Enable editor styles
+        add_theme_support( 'editor-styles' );
+        add_editor_style( 'editor-style.css' );
+
+        // Enable responsive embeds
+        add_theme_support( 'responsive-embeds' );
+    }
+    
     /**
      * remove the emoji style from frontend.
      * 
