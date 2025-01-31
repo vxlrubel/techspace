df3467b01233a2fa7c99db9b0c5202c6cb9cb37e
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-31 22:55:44 +0600

    create portfolio template

---
+++ page-portfolio.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: PortFolio Page
+ */
+
+//  directly access denied
+defined('ABSPATH') || exit;
+
+get_header(); ?>
+
+<main class="main-area" id="mainContent">
+    <div class="container">
+        <h2 class="fs-1 mt-3 text-center">PortFolio</h2>
+    </div>
+</main>
+<?php get_footer(); ?>
