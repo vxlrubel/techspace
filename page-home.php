f6ca1a42b9abf063ff4743582e3e9c07a6297288
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-31 22:53:55 +0600

    create home template

---
+++ page-home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Home Page
+ */
+
+//  directly access denied
+defined('ABSPATH') || exit;
+
+get_header(); ?>
+
+<main class="main-area" id="mainContent">
+    <div class="container">
+        <h2 class="fs-1 mt-3 text-center">Homepage</h2>
+    </div>
+</main>
+<?php get_footer(); ?>
